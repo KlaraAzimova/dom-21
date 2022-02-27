44fe099d702317689003f0f01ece0e90d86c8c3d
Klara <azimovaklara18@gmail.com>
2022-02-27 20:00:05 +0300

    Initial commit

---
+++ src/com/company/Days.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public enum Days {
+    DUISHOMBU,
+    SHEISHEMBI,
+    SHARSHEMBI,
+    BEISHEMBI,
+    JUMA,
+    ISHEMBI,
+    JEKSHEMBI;
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Days days = Days.DUISHOMBU;
+        System.out.println(days + " " + "Java teoria okuim... ");
+        Days days1 = Days.SHEISHEMBI;
+        System.out.println(days1 + " " + "Anglis tilin okuim... ");
+        Days days2 = Days.BEISHEMBI;
+        System.out.println(days2 + " " + "Soft Scills okuibuz... ");
+        Days days3 = Days.ISHEMBI;
+        System.out.println(days3 + " " + "Koshumcha session... ");
+        Days days4 = Days.JEKSHEMBI;
+        System.out.println(days4 + " " + "Es alabyz... ");
+
+    }
+}
